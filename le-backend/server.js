6a3718f12926e2a013e2ce579073ee2dd3da5486
Ali-Hasnain-17 <alihascs830@gmail.com>
2024-05-14 23:39:54 +0500

    changes

--- le-backend/server.js
+++ le-backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
@@ -8,7 +9,7 @@ app.use(
     origin: "http://localhost:5173",
   })
 );
-
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
